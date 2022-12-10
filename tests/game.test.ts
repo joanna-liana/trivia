96fe3528b15b537c9bdd9004a7bb2d582bf3cece
Joanna Liana <joanna_liana@outlook.com>
2022-12-10 19:15:56 +0100

    Remove pointless test

--- tests/game.test.ts
+++ tests/game.test.ts
@@ -3,7 +3,5 @@ import { describe, it } from 'mocha';
 import { GameRunner } from '../src/game-runner';
 
 describe('The test environment', () => {
-  it('should pass', () => {
-    expect(true).to.be.true;
-  });
+
 });
