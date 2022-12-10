1fce162ef433859c6c02dd6e1e1010cedddd1333
Joanna Liana <joanna_liana@outlook.com>
2022-12-10 19:54:13 +0100

    Add very basic characterisation tests for runner

--- tests/game.test.ts
+++ tests/game.test.ts
@@ -1,7 +1,26 @@
-import { expect } from 'chai';
+import * as sinon from 'sinon';
 import { describe, it } from 'mocha';
-import { GameRunner } from '../src/game-runner';
+import { GameRunner } from '../src/game-runner-v2';
+import { Game } from '../src/game';
 
 describe('The test environment', () => {
+  it("characterises the current behaviour - 1", () => {
+    const game = sinon.createStubInstance(Game);
+    const shouldContinueOnWrongAnswer = () => true;
 
+    GameRunner.main(game, shouldContinueOnWrongAnswer);
+
+    sinon.assert.calledOnce(game.wrongAnswer);
+    sinon.assert.calledOnce(game.roll);
+  })
+
+  it("characterises the current behaviour - 2", () => {
+    const game = sinon.createStubInstance(Game);
+    const shouldContinueOnWrongAnswer = () => false;
+
+    GameRunner.main(game, shouldContinueOnWrongAnswer);
+
+    sinon.assert.calledOnce(game.wasCorrectlyAnswered);
+    sinon.assert.calledOnce(game.roll);
+  })
 });
