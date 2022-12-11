b7dd18e3c60f9d2935141a61555d091fe9c03dd9
Joanna Liana <joanna_liana@outlook.com>
2022-12-11 09:24:53 +0100

    Add test for changing game continuation rules

--- tests/game.test.ts
+++ tests/game.test.ts
@@ -25,4 +25,25 @@ describe("Game runner", () => {
     sinon.assert.calledOnce(game.wasCorrectlyAnswered);
     sinon.assert.calledOnce(game.roll);
   })
+
+  it("can change the condition for continuing the game after each round", () => {
+    // given
+    const game = sinon.createStubInstance(Game);
+    const shouldContinueOnWrongAnswer = sinon.stub();
+
+    shouldContinueOnWrongAnswer.onFirstCall().returns(false);
+    game.wasCorrectlyAnswered.onFirstCall().returns(true);
+
+    shouldContinueOnWrongAnswer.onSecondCall().returns(true);
+    game.wrongAnswer.returns(true);
+
+    shouldContinueOnWrongAnswer.onThirdCall().returns(false);
+    game.wasCorrectlyAnswered.onSecondCall().returns(false);
+
+    // when
+    GameRunner.main(game, shouldContinueOnWrongAnswer);
+
+    // then
+    sinon.assert.calledThrice(game.roll);
+  })
 });
