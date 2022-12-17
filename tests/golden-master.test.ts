4ba24e4ef17f176ea6ef631dfb594d8b5d990548
Joanna Liana <joanna_liana@outlook.com>
2022-12-17 07:39:04 +0100

    Add tests for penalty box logic, exposing issue with original implementation

--- tests/golden-master.test.ts
+++ tests/golden-master.test.ts
@@ -4,36 +4,38 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import sinon from 'sinon';
 
-import { GameRunner } from '../src/game-runner-v2';
+import { AnyGame, GameRunner } from '../src/game-runner-v2';
 import { Game as GameV2 } from '../src/game-v2';
+import { Game } from '../src/game';
 
 describe("Golden master", () => {
   const NUMBER_OF_CHECKS = 10;
 
+  let generator: seedrandom.PRNG;
+
+  const silentLog = sinon
+    .stub(console, 'log')
+    .callsFake(() => null);
+
+  function combineLogs() {
+    const combinedLogs = silentLog.getCalls().reduce((allLogs, currentCall) => {
+      allLogs += '\n' + currentCall.args[0];
+
+      return allLogs;
+    }, '');
+
+    silentLog.resetHistory();
+
+    return combinedLogs;
+  }
+
+  function getGenerator(seed: number) {
+    return seedrandom(String(seed))
+  }
+
+
   it("compares the logs of two game implemetations", () => {
     // given
-    let generator: seedrandom.PRNG;
-
-    const silentLog = sinon
-      .stub(console, 'log')
-      .callsFake(() => null);
-
-    function combineLogs() {
-      const combinedLogs = silentLog.getCalls().reduce((allLogs, currentCall) => {
-        allLogs += '\n' + currentCall.args[0];
-
-        return allLogs;
-      }, '');
-
-      silentLog.resetHistory();
-
-      return combinedLogs;
-    }
-
-    function getGenerator(seed: number) {
-      return seedrandom(String(seed))
-    }
-
     const shouldContinueOnWrongAnswer = () => !!Math.floor(generator() * 2);
     const getNumberToRoll = () => Math.floor(generator() * 7);
 
@@ -56,4 +58,58 @@ describe("Golden master", () => {
       expect(allLogsOriginal).to.equal(allLogsRefactored);
     }
   });
+
+  describe('penalty box logic', () => {
+    function putInPenaltyBoxAndGetOut(game: AnyGame, seed: number) {
+      generator = getGenerator(seed);
+
+      // Not entirely convinced that the randomisation is needed here
+      const randomInt = Math.floor(generator() * 100)
+
+      const numberThatGetsOutOfPenaltyBox = randomInt % 2 === 0 ? randomInt + 1 : randomInt;
+      const numberThatDoesNotGetOutOfPenaltyBox = numberThatGetsOutOfPenaltyBox + 1;
+
+      game.add("SINGLE PLAYER");
+      game.roll(numberThatGetsOutOfPenaltyBox);
+      game.wrongAnswer();
+      game.roll(numberThatGetsOutOfPenaltyBox);
+      game.wasCorrectlyAnswered();
+      game.roll(numberThatDoesNotGetOutOfPenaltyBox);
+      game.wasCorrectlyAnswered();
+    }
+
+    it("compares the logs of two game implementations", () => {
+      for (let seed = 0; seed < NUMBER_OF_CHECKS; seed += 1) {
+        // when
+        putInPenaltyBoxAndGetOut(new Game(), seed);
+
+        const allLogsOriginal = combineLogs();
+
+        // and when
+        putInPenaltyBoxAndGetOut(new GameV2(), seed);
+
+        const allLogsRefactored = combineLogs()
+
+        // then
+        expect(allLogsOriginal).to.equal(allLogsRefactored);
+      }
+    })
+
+    it("ensures that a player can successfully get out of the penalty box", () => {
+      function expectPlayerToHaveLeftPenaltyBox(gameLogs: string) {
+        const correctAnswersCount = gameLogs.match(/Answer was correct!!!!/g)?.length;
+        expect(correctAnswersCount).to.equal(2);
+      }
+
+      for (let seed = 0; seed < NUMBER_OF_CHECKS; seed += 1) {
+        // when
+        putInPenaltyBoxAndGetOut(new Game(), seed);
+
+        // then
+        const gameLogs = combineLogs();
+        expectPlayerToHaveLeftPenaltyBox(gameLogs);
+      }
+    })
+  })
+
 });
