d67aed4c5885c11ecb6c897602033cb54137772d
Joanna Liana <joanna_liana@outlook.com>
2022-12-18 09:57:21 +0100

    Extract category names to constants

--- src/Questions.ts
+++ src/Questions.ts
@@ -1,3 +1,8 @@
+const POP = 'Pop';
+const SCIENCE = 'Science';
+const SPORTS = 'Sports';
+const ROCK = 'Rock';
+
 export class Questions {
   private popQuestions: Array<string> = [];
   private scienceQuestions: Array<string> = [];
@@ -6,10 +11,10 @@ export class Questions {
 
   constructor() {
     for (let i = 0; i < 50; i++) {
-      this.popQuestions.push("Pop Question " + i);
-      this.scienceQuestions.push("Science Question " + i);
-      this.sportsQuestions.push("Sports Question " + i);
-      this.rockQuestions.push("Rock Question " + i);
+      this.popQuestions.push(POP + " Question " + i);
+      this.scienceQuestions.push(SCIENCE + " Question " + i);
+      this.sportsQuestions.push(SPORTS + " Question " + i);
+      this.rockQuestions.push(ROCK + " Question " + i);
     }
   }
 
@@ -28,15 +33,15 @@ export class Questions {
   private currentCategoryQuestions(magicNumber: number): string[] {
     const category = this.currentCategory(magicNumber);
 
-    if (category == 'Pop') {
+    if (category == POP) {
       return this.popQuestions;
     }
 
-    if (category == 'Science') {
+    if (category == SCIENCE) {
       return this.scienceQuestions!;
     }
 
-    if (category == 'Sports') {
+    if (category == SPORTS) {
       return this.sportsQuestions;
     }
 
@@ -45,17 +50,17 @@ export class Questions {
 
   public currentCategory(magicNumber: number): string {
     if ([0, 4, 8].includes(magicNumber)) {
-      return 'Pop';
+      return POP;
     }
 
     if ([1, 5, 9].includes(magicNumber)) {
-      return 'Science';
+      return SCIENCE;
     }
 
     if ([2, 6, 10].includes(magicNumber)) {
-      return 'Sports';
+      return SPORTS;
     }
 
-    return 'Rock';
+    return ROCK;
   }
 }
