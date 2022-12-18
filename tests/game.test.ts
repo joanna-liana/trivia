a63370e404504fadb4e52136c8017de281786ff0
Joanna Liana <joanna_liana@outlook.com>
2022-12-18 10:28:25 +0100

    Fix adding players in failing test

--- tests/game.test.ts
+++ tests/game.test.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { Game } from '../src/game-v2';
+import { PlayerName } from '../src/Player';
 
 describe("Game", () => {
   it("The maximum number of player can be limited", () => {
@@ -9,9 +10,9 @@ describe("Game", () => {
     const game = new Game({ maxPlayers: MAX_PLAYERS });
 
     for (let i = 0; i < 6; i++) {
-      game.add(`PLAYER ${i}`);
+      game.add(<PlayerName>`PLAYER ${i}`);
     }
 
-    expect(() => game.add('EXTRA PLAYER')).to.throw(PlayerLimitReached)
+    expect(() => game.add(<PlayerName>'EXTRA PLAYER')).to.throw(PlayerLimitReached)
   })
 });
