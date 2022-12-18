9a31b2815e4f30b5108110b63385bde4ad87e9eb
Joanna Liana <joanna_liana@outlook.com>
2022-12-18 09:40:31 +0100

    Make Player.place readonly

--- src/Player.ts
+++ src/Player.ts
@@ -7,19 +7,22 @@ type Opaque<A, B> = A & {
 export type PlayerName = Opaque<string, "PlayerName">;
 
 export class Player {
-  place: number = 0;
+  #place: number = 0;
   purse: number = 0;
-
   isInPenaltyBox: boolean = false;
+
+  get place(): number {
+    return this.#place;
+  }
 
   constructor(public readonly name: PlayerName, private readonly maxPlace = 12) {
   }
 
   move(places: number) {
-    this.place += places;
+    this.#place += places;
 
-    if (this.place >= this.maxPlace) {
-      this.place -= this.maxPlace;
+    if (this.#place >= this.maxPlace) {
+      this.#place -= this.maxPlace;
     }
   }
 }
